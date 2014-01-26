ef4ffc8f1e3e8fb4dcf31c0089eafd10073f2946
linux_china <linux_china@hotmail.com>
2014-01-27 01:55:36 +0800

    jenv plugin

---
+++ lib/vagrant-jenv.rb
@@ -0,0 +1,30 @@
+module VagrantPlugins
+  module Jenv
+    class Plugin < Vagrant.plugin("2")
+
+      name "Vagrant jenv plugin"
+
+      description <<-DESC
+      Vagrant jenv plugin
+      DESC
+
+      command "jenv-info" do
+        require_relative "vagrant-jenv/command"
+        Command
+      end
+
+
+      config(:jenv, :provisioner) do
+        require_relative "vagrant-jenv/config"
+        Config
+      end
+
+      provisioner :jenv do
+        require_relative "vagrant-jenv/provisioner"
+        Provisioner
+      end
+
+    end
+
+  end
+end
