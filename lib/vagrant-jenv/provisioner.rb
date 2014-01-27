13ef865179a566d39ab656198d54dae4dea31c45
linux_china <linux_china@hotmail.com>
2014-01-27 10:12:22 +0800

    implement install candidate logic

--- lib/vagrant-jenv/provisioner.rb
+++ lib/vagrant-jenv/provisioner.rb
@@ -2,8 +2,16 @@ module VagrantPlugins
   module Jenv
     class Provisioner < Vagrant.plugin("2", "provisioner")
       def provision
-        #@machine.communicate.execute("ls -al")
-        puts "#{config.candidates.join(' ')}"
+        @machine.communicate.execute('if [[ ! -d "${HOME}/.jenv" ]]; then curl -L -s get.jenv.io | bash ; fi') do |type, data|
+          @machine.env.ui.info data
+        end
+        @machine.env.ui.info "Installing jenv candidates...}"
+        @machine.communicate.execute('jenv config auto true')
+        @machine.communicate.execute('jenv repo update')
+        config.candidates.each do |pair|
+          @machine.communicate.execute("jenv install #{pair}")
+          @machine.env.ui.info "#{pair} installed"
+        end
       end
     end
   end
